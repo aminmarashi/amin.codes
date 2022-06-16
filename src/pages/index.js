cb1003be774259543c88c219bf0abe391770618b
Amin <me@amin.codes>
2022-06-16 08:44:11 +0200

    Add my profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
+import { Link } from "gatsby";
 import * as React from "react";
+import Amin from '../images/amin.jpeg';
 import '../styles/global.css';
 
 const IndexPage = () => (
@@ -12,13 +14,16 @@ const IndexPage = () => (
     >
       <main>
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">Pricing</h2>
-            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-              Take control of your team.
-            </p>
+          <div className="text-center grid grid-cols-1 gap-4 justify-items-center">
+            <img className="rounded-full" alt="Amin's profile" src={Amin}/>
+            <h3 className="mt-1 font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Amin Codes
+            </h3>
             <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-              Start building for free, then add a site plan to go live. Account plans unlock additional features.
+              A software developer, enjoying stroopwafels in Amsterdam and writing code.
+            </p>
+            <p>
+              Write to <Link to="mailto:me@amin.codes">me@amin.codes</Link> or follow me on <Link to="https://github.com/aminmarashi">GitHub</Link>
             </p>
           </div>
         </div>
