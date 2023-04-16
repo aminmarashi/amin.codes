5d1c449fd283ed96e7beb459c4ce37df63f4450d
aminmarashi <aminmarashi@users.noreply.github.com>
2023-04-16 08:21:00 +0000

    deploy: f41636a739fba14e1aef93cc04afa73e6c9659b3

--- component---src-pages-404-js-f28a132a50e73eef3bea.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkamin_codes=self.webpackChunkamin_codes||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294),l=n(5592),a=n(262);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-f28a132a50e73eef3bea.js.map
